1dec64f713f8dcd9b3eaea9297ad6fd8118366cf
VrutikShah <vrutik.vs@gmail.com>
2021-05-08 23:50:12 +0530

    Physics working. Equations of Motion done

--- main.py
+++ main.py
@@ -3,39 +3,32 @@ import numpy as np
 import cv2
 from utils import *
 import math
-from scipy.integrate import quad
-
-dim_x = 600
-dim_y = 400
-frame = np.ones((dim_y,dim_x,3),np.uint8)*255
-a = 0
-l = 50
-x,y = 100,100 #center
-x1 = x-l
-x2 = x+l
-y1 = y
-y2 = y
 
 
-grav = 9.8
 
 
 class quad:
     def __init__(self):
         self.l = 50
         self.thc = 3
+        self.m = 0.18
+        self.grav = 9.8
+        self.I = 2.5*1e-4
 
-        self.x = 200
-        self.y = 300
+        self.x = 300
+        self.y = 200
         self.x1 = self.x-self.l
         self.y1 = self.y
         self.x2 = self.x+self.l
         self.y2 = self.y
         
-        self.Phi = 0
+        self.Phi = -20
 
-        self.f1 = 0
-        self.f2 = 0
+        self.f1 = 0.5
+        self.f2 = 0.5
+
+        self.u2 = self.f2*self.l - self.f1*self.l
+        self.u1 = self.f1 + self.f2
 
         # Linear velocities
         self.v = 0          #Lateral velocity
@@ -51,33 +44,11 @@ class quad:
         self.xdot = math.cos(deg2rad(self.Phi)) * self.w - math.sin(deg2rad(self.Phi)) * self.v
         self.ydot = - math.sin(deg2rad(self.Phi)) * self.v - math.cos(deg2rad(self.Phi)) * self.w
 
-    def getPhi(self):
-        return self.Phi
-
-    def moveY(self,frame,units):
-        cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,255,255),self.thc)
-        self.y1-=units
-        self.y2-=units
-        cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,0,0),self.thc)
     
-    def moveX(self,frame,units):
+    def newState(self,frame):
         cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,255,255),self.thc)
-        self.x1+=units
-        self.x2+=units
-        cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,0,0),self.thc)
-    
-    def rotate(self,frame,degrees):
-        cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,255,255),self.thc)
-        self.Phi += degrees
         self.Phi %= 360
-
-        # rad = deg2rad(degrees)
         rad = deg2rad(self.Phi)
-        # x1 = round((self.x1 - self.x) * math.cos(rad) - (self.y1 - self.y) * math.sin(rad) + self.x)
-        # x2 = round((self.x2 - self.x) * math.cos(rad) - (self.y2 - self.y) * math.sin(rad) + self.x)
-        # y1 = round((self.x1 - self.x) * math.sin(rad) + (self.y1 - self.y) * math.cos(rad) + self.y)
-        # y2 = round((self.x2 - self.x) * math.sin(rad) + (self.y2 - self.y) * math.cos(rad) + self.y)
-
         x1 = round(- self.l * math.cos(rad) + self.x)
         x2 = round(+ self.l * math.cos(rad) + self.x)
         y1 = round(+ self.l * math.sin(rad) + self.y)
@@ -90,34 +61,122 @@ class quad:
     
         cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,0,0),self.thc)
     
+
+    
+    def getPhi(self):
+        return self.Phi
+
+    def moveY(self,frame,units):
+        cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,255,255),self.thc)
+        self.y1-=units
+        self.y2-=units
+        self.y = (self.y1+self.y2)/2
+        cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,0,0),self.thc)
+    
+    def moveX(self,frame,units):
+        cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,255,255),self.thc)
+        self.x1+=units
+        self.x2+=units
+        self.x = (self.x1+self.x2)/2
+        cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,0,0),self.thc)
+
+    def rotate(self,frame,degrees):
+        cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,255,255),self.thc)
+        self.Phi += degrees
+        self.Phi %= 360
+
+        # rad = deg2rad(degrees)
+        rad = deg2rad(self.Phi)
+        x1 = round(- self.l * math.cos(rad) + self.x)
+        x2 = round(+ self.l * math.cos(rad) + self.x)
+        y1 = round(+ self.l * math.sin(rad) + self.y)
+        y2 = round(- self.l * math.sin(rad) + self.y)
+        
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+    
+        cv2.line(frame,(self.x1,self.y1),(self.x2,self.y2),(255,0,0),self.thc)
+    
+        # x1 = round((self.x1 - self.x) * math.cos(rad) - (self.y1 - self.y) * math.sin(rad) + self.x)
+        # x2 = round((self.x2 - self.x) * math.cos(rad) - (self.y2 - self.y) * math.sin(rad) + self.x)
+        # y1 = round((self.x1 - self.x) * math.sin(rad) + (self.y1 - self.y) * math.cos(rad) + self.y)
+        # y2 = round((self.x2 - self.x) * math.sin(rad) + (self.y2 - self.y) * math.cos(rad) + self.y)
+
+    
+
+
+
+def normalForces(obj):
+    return -(obj.f1 + obj.f2)/obj.m + obj.grav * math.cos(deg2rad(obj.Phi)) - obj.p * obj.v
+
+def lateralForces(obj):
+    return obj.grav * math.sin(deg2rad(obj.Phi)) + obj.p * obj.w
+
+def update(obj,t):
+    obj.v = t * lateralForces(obj) + obj.v
+    obj.w = t * normalForces(obj) + obj.w
+    obj.p = t * obj.u2 / obj.I + obj.p
+    obj.Phi = obj.p * t + obj.Phi
+
+    obj.xdot = -math.cos(deg2rad(obj.Phi)) * obj.v + math.sin(deg2rad(obj.Phi)) * obj.w
+    obj.ydot = math.sin(deg2rad(obj.Phi)) * obj.v + math.cos(deg2rad(obj.Phi)) * obj.w
+
+    obj.y = obj.ydot * t + obj.y
+    obj.x = obj.xdot * t + obj.x
+
+    obj.newState(frame)
+    # obj.Phi -= 0.2
+    print('w: {}, v: {}, xdot: {}'.format(obj.w,obj.v,obj.xdot))
+
+
+def statUpdate(frame,obj,text1,text2):
+    cv2.putText(frame,text1,(10,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,255,255),1)
+    text1 = 'phi: ' + str(obj.getPhi())
+    cv2.putText(frame,text1,(10,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,0),1)
+
+    cv2.putText(frame,text2,(10,50),cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,255,255),1)
+    text2 = 'y: ' + str(obj.y)
+    cv2.putText(frame,text2,(10,50),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,0),1)
+    
+    return text1,text2
+    
+
+dim_x = 600
+dim_y = 700
+frame = np.ones((dim_y,dim_x,3),np.uint8)*255
+grav = 9.8
+
 obj = quad()
 
-text = 'phi: ' + str(obj.getPhi())
-cv2.putText(frame,text,(10,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,0),1)
+text1 = 'phi: ' + str(obj.getPhi())
+text2 = 'y: 0'
 
-cv2.line(frame,(x1,y1),(x2,y2),(255,0,0),3)
-deg = 1
-count = 0
+t = 0
 
-def netThrust():
-    return obj.f1 + obj.f2 - grav
 
-def update():
-    thrust = netThrust()
-    y_vel = 
 
+
+while t<30:
+    # if obj.getPhi() <= 89:
+    # cv2.putText(frame,text,(10,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,255,255),1)
+    # text = 'phi: ' + str(obj.getPhi())
+    # cv2.putText(frame,text,(10,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,0),1)
+    # y_vel,y_pos = basic_update(0.1,accn,y_pos,y_vel)
+    
+    text1,text2 = statUpdate(frame,obj,text1,text2)
+    
+    update(obj,0.1)
+    t+=.1
+
+    time.sleep(0.02)
+    cv2.imshow('an',frame)
+    k = cv2.waitKey(10)
+    if k ==27:
+        break
 
 while True:
-    # if obj.getPhi() <= 89:
-    cv2.putText(frame,text,(10,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,255,255),1)
-    text = 'phi: ' + str(obj.getPhi())
-    cv2.putText(frame,text,(10,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,0),1)
-    obj.moveY(frame,2)
-    obj.moveX(frame,1)
-    obj.rotate(frame,1)
-    # time.sleep(0.1)
-    cv2.imshow('an',frame)
-    
     k = cv2.waitKey(10)
     if k ==27:
         break
--- motion.py
+++ motion.py
@@ -1,25 +1,27 @@
-from scipy.integrate import quad, dblquad
+# from scipy.integrate import quad, dblquad
 import matplotlib.pyplot as plt
 p = 0
 v = 0
-accn = 2
+accn = .02
 t = 0
 plt.axis([0,10,0,1])
 
 def integrand(x,val):
     return val
 
-def vel(accn,t1,t2):
-    return quad(integrand,t1,t2,args=(accn))
+# def vel(accn,t1,t2):
+#     return quad(integrand,t1,t2,args=(accn))
 
-def pos(p,t1,t2):
-    return dblquad(integrand,t1,t2,,args=(p))
+# def pos(p,t1,t2):
+#     return dblquad(integrand,t1,t2,,args=(p))
 
 def update(accn,p,v,t1):
-    v_temp,_ = vel(accn,t1,t1+0.1)
-    v += v_temp
-    x_temp,_ = pos(p,t1,t1+0.1)
-    p += x_temp
+    # v_temp,_ = vel(accn,t1,t1+0.1)
+    # v += v_temp
+    # x_temp,_ = pos(p,t1,t1+0.1)
+    # p += x_temp
+    v = accn*t1 + 0
+    p = v*t1 + 0
     return p
 
 
@@ -28,6 +30,6 @@ while t<10:
     p = update(accn,p,v,t)
     plt.scatter(t,p)
     plt.pause(0.05)
-    t+=0.1
+    t+=1
 
 plt.show()
